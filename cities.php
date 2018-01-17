021ad361b91903774dfabbad30652dbd09cae69b
jedz <jedzioski@gmail.com>
2018-01-17 17:49:04 +0100

    poc cities == wawa

---
+++ cities.php
@@ -0,0 +1,4 @@
+<?php
+$wawa = false;
+$wawa = true;
+$limit = " AND latitude > (52.22967560000001-0.107917) AND latitude < (52.22967560000001+0.107917) AND longitude > (21.012228700000037-0.175848) AND longitude < (21.012228700000037+0.175848)";
